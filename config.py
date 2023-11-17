5be96cffd0e2eef97a699b8d2b27fc6403d59dab
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-11-17 11:36:50 +0500

    feat: improved configuration

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
+import sys
+import tomli
 from dotenv import load_dotenv
+
+MODES = ["ip", "script"]
+
+class Router:
+    def __init__(self, addr, user, passwd, command):
+        self.address = addr
+        self.user = user
+        self.password = passwd
+        self.command = command
 
 class Config:
     def __init__(self):
@@ -8,20 +19,68 @@ class Config:
         if os.path.exists(dotenv_path):
             load_dotenv(dotenv_path)
 
-        self.TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+        with open("config.toml", mode="rb") as fp:
+            config = tomli.load(fp)
 
-        self.TELEGRAM_ALLOWED_USERS = os.environ.get("TELEGRAM_ALLOWED_USERS").split(";") if os.environ.get("TELEGRAM_ALLOWED_USERS") else []
-        if isinstance(self.TELEGRAM_ALLOWED_USERS, str):
-            self.TELEGRAM_ALLOWED_USERS = [self.TELEGRAM_ALLOWED_USERS]
+        if not config:
+            raise ValueError("Config file not found")
 
-        self.CONFIRMATION_CODE = os.environ.get("CONFIRMATION_CODE")
+        # TODO: Validate config schema
+        print(config)
 
-        self.MT_IP_ADDRESS = os.environ.get("MT_IP_ADDRESS")
-        self.MT_USER = os.environ.get("MT_USER")
-        self.MT_PASSWORD = os.environ.get("MT_PASSWORD")
-        self.COMMAND = os.environ.get("COMMAND")
-        self.RESTRICT_ADDRESSES = os.environ.get("RESTRICT_ADDRESSES").split(";") if os.environ.get("RESTRICT_ADDRESSES") else []
-        self.SCRIPT = os.environ.get("SCRIPT")
+        def check_var(name, val, typ):
+            if not val or not type(val) is typ:
+                print(f"{name} is invalid: required and must be of type {typ}", file=sys.stderr)
+                exit(1)
+                
 
+        check_var("telegram", config.get("telegram"), dict)
+        check_var("security", config.get("security"), dict)
+        check_var("routers", config.get("routers"), dict)
+
+
+        self.TELEGRAM_TOKEN = config["telegram"].get("token")
+        check_var("telegram.token", self.TELEGRAM_TOKEN, str)
+
+        self.TELEGRAM_ALLOWED_USERS = config["telegram"].get("users")
+        check_var("telegram.users", self.TELEGRAM_ALLOWED_USERS, list)
+
+        self.CONFIRMATION_CODE = config["security"].get("password")
+        check_var("security.password", self.CONFIRMATION_CODE, str)
+
+
+        routers = config["routers"]
+        self.ROUTERS = []
+        for name in routers:
+            r = routers[name]
+
+            addr = r.get("address")
+            check_var(f"{name}.address", addr, str)
+
+            user = r.get("user")
+            check_var(f"{name}.user", user, str)
+
+            passwd = r.get("password")
+            check_var(f"{name}.password", passwd, str)
+
+            cmd = r.get("command")
+            check_var(f"{name}.command", cmd, dict)
+
+            cmd_mode = cmd.get("mode")
+            check_var(f"{name}.command.mode", cmd_mode, str)
+
+            if cmd_mode not in MODES:
+                print(f"command mode must be either ip or script, but {cmd_mode} provided", file=sys.stderr)
+                exit(1)
+
+            if cmd_mode == "ip":
+                addresses = cmd.get("addresses")
+                check_var(f"{name}.command.addresses", addresses, list)
+
+            if cmd_mode == "script":
+                name_ = cmd.get("name")
+                check_var(f"{name}.command.name", name_, str)
+
+            self.ROUTERS.append(Router(addr, user, passwd, cmd))
 
 config = Config()
--- mikrotik.py
+++ mikrotik.py
@@ -2,6 +2,18 @@ import subprocess
 from config import config
 
 def RestrictAccess():
+    for r in config.ROUTERS:
+        if r.command["mode"] == "ip":
+            result = subprocess.run(["scripts/restrict", r.address, r.user, r.password, r.command["mode"], *r.command["addresses"]], stderr=subprocess.PIPE, text=True)
+        elif r.command["mode"] == "script":
+            result = subprocess.run(["scripts/restrict", r.address, r.user, r.password, r.command["mode"], r.command["name"]], stderr=subprocess.PIPE, text=True)
+        else:
+            print("unreachable")
+            return
+        print(result.stderr)
+        
+
+def RestrictAccessOld2():
     if config.COMMAND == "ip":
         result = subprocess.run(["scripts/restrict", config.MT_IP_ADDRESS, config.MT_USER, config.MT_PASSWORD, config.COMMAND, *config.RESTRICT_ADDRESSES], stderr=subprocess.PIPE, text=True)
     elif config.COMMAND == "script":
